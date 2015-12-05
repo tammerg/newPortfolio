b133d9d845bd19d07ee23b6595c6fd2b77915a3c
Tammer Galal <tg385@scarletmail.rutgers.edu>
2015-12-04 20:04:45 -0500

    Used the github API and AJAX to dynamically show my repos on the repo.html page/tab

--- script/repos.js
+++ script/repos.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
   $.ajax({
-    type: "GET",
+    type:"GET",
     url: "https://api.github.com/users/tammerg/repos",
     success: function(repos){
-      console.log(repos);
-    }
-    error: function(jqXHR, textStatus, errorThrown){
-      console.log(jqXHR);
-      console.log(textStatus);
-      console.log(errorThrown);
-    }
-
-   })
+      for (i = 0; i < repos.length; i++){
+        $(".list-group").append("<p>" + repos[i].name + "</p>")
+      }   
+    },
+  })
 });
