5d1adeafe967064b59b86edb2deff1224042f73c
Tammer Galal <tg385@scarletmail.rutgers.edu>
2015-12-04 20:33:52 -0500

    Repos are dynamically loading and page and now clickable to show the sha.

--- script/repos.js
+++ script/repos.js
@@ -4,8 +4,23 @@ $(document).ready(function(){
     url: "https://api.github.com/users/tammerg/repos",
     success: function(repos){
       for (i = 0; i < repos.length; i++){
-        $(".list-group").append("<p>" + repos[i].name + "</p>")
-      }   
+        var newRepoUrl = buildRepoUrl(repos[i])
+        $(".list-group").append(newRepoUrl)
+      } 
+
     },
-  })
+  });
+
+  function buildRepoUrl (repoData) {
+    var commitsApiUrl = "https://api.github.com/repos/";
+    commitsApiUrl += repoData.owner.login + "/";
+    commitsApiUrl += repoData.name + "/commits";
+
+    var newLink = $("<a>")
+      .attr("href", commitsApiUrl)
+      .addClass("list-group-item")
+      .append(repoData.name)
+    return newLink;
+  }
+
 });
